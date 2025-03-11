e8590de6c0654badd792d9473c9bad15ed6dda81
Muhammed Taha <zedanmhmd41@gmail.com>
2025-03-11 04:09:38 +0200

    fix name (content.js/css => Content.js/css)

--- src/View/Components/Dashboard/Content/content.js
+++ src/View/Components/Dashboard/Content/content.js
@@ -113,26 +113,22 @@ const Content = ({ userRole }) => {
           />
         </div>
 
-               {/* row 3 */}
-       <div className="row">
-         <Box
-           title="Test"
-           contentBox="This is a test1"
-           image={img}
-           gridRow="span 10"
-         />
-         <Box
-           title="Test"
-           contentBox="This is a test2"
-           image={img}
-           gridColumn="span 8"
-           gridRow="span 10"
-          
-
-         />
-       </div>
-
-        
+        {/* row 3 */}
+        <div className="row">
+          <Box
+            title="Test"
+            contentBox="This is a test1"
+            image={img}
+            gridRow="span 10"
+          />
+          <Box
+            title="Test"
+            contentBox="This is a test2"
+            image={img}
+            gridColumn="span 8"
+            gridRow="span 10"
+          />
+        </div>
       </>
     ),
     lecturer: (
@@ -165,7 +161,7 @@ const Content = ({ userRole }) => {
   );
 };
 
-export default Content
+export default Content;
 
 //   return (
 //     <div className="content">
@@ -227,7 +223,6 @@ export default Content
 //           image={img}
 //           gridColumn="span 8"
 //           gridRow="span 10"
-          
 
 //         />
 //       </div>
